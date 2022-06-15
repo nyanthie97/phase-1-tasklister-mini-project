cc244ccf837eb1b727c417e76cb3e4c410613645
Nyanthie Mayang Gatluak <nyanthie.mayang@student.moringaschool.com>
2022-06-15 09:19:03 +0300

    update src

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  
+const form = document.querySelector('#create-task-form')
+
+form.addEventListener('submit', (event) => {
+
+  const newInput = document.querySelector('#new-task-description').value
+  const newTask = document.querySelector('#tasks')
+  const newList = document.createElement('li')
+
+  newTask.append(newList)
+  newList.innerText = newInput
+
+  event.preventDefault()
+
+
+})
 });
